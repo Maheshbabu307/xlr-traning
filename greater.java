0a6245d242ac561f8f8dfafa22bc9001790380a0
Maheshbabu307 <2311cs010307@mallareddyuniversity.ac.in>
2025-01-02 14:21:22 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,17 @@
+public class Main {
+    public class GreaterNumber {
+        public static void main(String[] args) {
+            int num1 = 10; 
+            int num2 = 20; 
+    
+            if (num1 > num2) {
+                System.out.println("The greater number is: " + num1);
+            } else if (num2 > num1) {
+                System.out.println("The greater number is: " + num2);
+            } else {
+                System.out.println("Both numbers are equal.");
+            }
+        }
+    }
+    
+}
